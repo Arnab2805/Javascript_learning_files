0b85a5155a0c2c443bd07cc66f0a81a8d0fcf5ab
ARNAB MITRA <100119297+Arnab2805@users.noreply.github.com>
2024-07-08 17:25:01 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code in a latest version
+
+
+console.log(typeof "Arnab")
+console.log(typeof 1)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+/* 
+Output
+--------------------
+string
+number
+boolean
+object
+undefined
+
+*/
